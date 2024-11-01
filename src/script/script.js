af0ea60c24cbee04b12d78f63940dc1d62b5dc2b
kbone8579 <a01056718579@gmail.com>
2024-11-02 02:39:52 +0900

    work

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,4 @@
+// swiper
 var swiper = new Swiper(".mySwiper", {
     loop: true,
     spaceBetween: 30,
@@ -5,4 +6,20 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+  });
+
+// 화면 확대 축소
+  $(document).ready(function () {
+    // Touch device support
+    $(".gallery-item").on("touchstart", function () {
+      $(".gallery-item").removeClass("active");
+      $(this).addClass("active");
+    });
+  
+    // Remove active class when clicking outside
+    $(document).on("touchstart", function (e) {
+      if (!$(e.target).closest(".gallery-item").length) {
+        $(".gallery-item").removeClass("active");
+      }
+    });
   });
